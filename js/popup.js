6e524dcaf3fa747be9011c11c4f0af5954a9c8ac
Marcio Antônio de Andrade Bortoloti <mbortoloti@uesb.edu.br>
2026-01-02 07:50:10 -0300

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  if (!localStorage.getItem("popupVisto")) {
-    document.getElementById("popup").style.display = "flex";
-  }
-});
-
 function fecharPopup() {
   document.getElementById("popup").style.display = "none";
   localStorage.setItem("popupVisto", "true");
